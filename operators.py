65cc215acf9bcd828896e00340d2011aa60fbc33
hugo-michel <orayito62@gmail.com>
2023-06-06 19:24:37 +0200

    operators

---
+++ operators.py
@@ -0,0 +1,46 @@
+# division entiere
+
+a = 10 //3 # variable = operande operateur operande
+print(a) # donne 3 (pas 3.33333)
+
+#puissance
+
+b = 2 ** 3 #2 * 2 * 2
+print(b)
+
+#modulo
+c = 17 % 5 #17/5 = (15/5) + 2
+print(c)
+
+#exemple modulo
+c = 50 % 24 #donne 2
+print(c)
+
+#nombre pair ou impair
+
+d = 53 # si reste aprés modulo 2 nombre impair
+print(53 % 2)
+e = 54 #si pas de reste modulo 2 nombre pair
+print(54 % 2 )
+
+#operateur de comparison
+
+print(123 == 123) #True
+print(123 == 42)  #False
+
+result = 123 == 42
+print(result)       #False
+
+print("abc" == "def") #False
+
+
+#operateur plus grand que 
+result2 = 123 > 42
+print(result2)
+
+#plus grand ou egal
+print(42 >= 42)
+
+#different de
+print(42 != 42) #false
+
