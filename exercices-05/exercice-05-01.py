05ee2b5f1c5442bcbc7198bfc6383585c61e5a3a
hugo-michel <orayito62@gmail.com>
2023-06-11 15:29:08 +0200

    code exo 5

---
+++ exercices-05/exercice-05-01.py
@@ -0,0 +1,15 @@
+# exo 5.1
+# Ajoutez de la documentation à la fonction suivante et vérifiez
+# qu'elle s'affiche correctement en utilisant la fonction help()
+
+# réponse 5.1
+
+def multiplication(a: float, b: float) -> float:
+    """renvoie le produit des nombres a et b
+    a float le nombre a
+    b float le nombre b
+    return float    
+    """
+    return a * b
+
+help(multiplication)
