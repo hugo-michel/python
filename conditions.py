33a4a4d114f7006cae9ec761dd52ee07d2b7ef35
hugo-michel <orayito62@gmail.com>
2023-06-12 15:32:29 +0200

    conditions

--- conditions.py
+++ conditions.py
@@ -34,5 +34,52 @@ print(f"{number1 = }")
 print(f"{number2 = }")
 if number1 < number2:
     print("number1 est plus petit que number2")
+elif number1 > number2:
+     print("number1 est plus grand que number2")
 else:
-    print("number1 est plus grand que number2")
+    print("number1 est egal à number2")
+
+
+# reecriture
+if number1 < number2:
+    print("number1 est plus petit que number2")
+else:
+    if number1 > number2:
+        print("number1 est plus grand que number2")
+    else:
+        print("number1 est egal à number2")
+
+
+#opérateurs booleens
+print(not True) #donne false
+print(not False) #donne true
+
+#Table de vérité  de not
+#A      not A
+#True   False
+#False  True
+
+# OU logique
+print(True or True)
+print(True or False)
+print(False or False)
+#table de vérité OR
+#A  B   AorB
+#T  T   T
+#T  F   T
+#F  T   T
+#F  F   F
+
+# AND
+print(True and True)
+print(True and False)
+print(False and False)
+#table vérité AND et #NAND not and
+#A  B   AandB   NAND
+#T  T   T       F
+#T  F   F       T
+#F  T   F       T
+#F  F   F       T
+
+
+
