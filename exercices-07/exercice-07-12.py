6badcad0f458a652d1bcf6bb5e00445b5885cf7e
hugo-michel <orayito62@gmail.com>
2023-06-13 16:49:34 +0200

    code exo 07

---
+++ exercices-07/exercice-07-12.py
@@ -0,0 +1,16 @@
+# exo 7.12
+# en utilisant une boucle for, on tire 100 fois un nombre entier `r` au hasard entre 1 et 10 inclus
+# utilisez une variable compteur `count` pour compter le nombre de fois où `r` est compris entre 2 et 9 inclus
+# affichez la variable `count` après la boucle
+
+import random
+
+# réponse 7.12
+
+count = 0
+
+for i in range(0, 99):
+    r = random.randint(1, 10)
+    if 2 <= r <= 9:
+        count += 1
+print(count)
---
+++ exercices-07/exercice-07-14.py
@@ -0,0 +1,10 @@
+# exo 7.14
+# en utilisant une boucle for, affichez tous les nombre pairs, de 1 à 100 inclus
+
+# réponse 7.14
+
+for i in range(1, 101):
+    if i % 2 == 0:
+        print(i)
+
+
